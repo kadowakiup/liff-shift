2c882c39b373c69f02e4d78d1b2707aad6a80dff
kadowakiup <kadowaki.universal.prime@gmail.com>
2026-06-23 13:45:11 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -964,13 +964,19 @@ actualStart = shift.actualStart || ""; // ★追加
             return;
           }
 
-          applyLocalAddShift(selectedDateStr, start, end, data.shiftId || "");
-          rerenderCurrentMonth(); // カレンダーを再描画して追加したシフトを表示させる
+          applyLocalAddShift(
+            selectedDateStr, 
+            start, 
+            end, 
+            data.shiftId || "", 
+            start, // ★ actualStart の代わりに、入力した start を入れる
+            end    // ★ actualEnd の代わりに、入力した end を入れる
+          );
+          
+          rerenderCurrentMonth();
 
-          // Anycrossで設定した "message" が優先して表示されます
           alert(data.message || "シフト追加が完了しました");
 
-          // ★修正：liff.closeWindow(); を消し、カレンダー画面に戻る処理にする
           detailView.style.display = "none";
           calendarView.style.display = "block";
           resetDetailState();
